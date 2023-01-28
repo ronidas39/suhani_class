ec809bab628f481a578ecc80b558afa53ed0aafd
ronidas39 <ronidas041987@gmail.com>
2023-01-28 16:46:21 +0530

    class 28 th jan

--- API_TUTORIAL/FASTAPI/TUTORIAL1/main.py
+++ API_TUTORIAL/FASTAPI/TUTORIAL1/main.py
@@ -1,6 +1,23 @@
 from fastapi import FastAPI
+import io
 
 app=FastAPI()
+
+
+def readFile(name):
+    found=0
+    with io.open("input.csv","r",encoding="utf-8")as f1:
+        data=f1.read()
+        f1.close()
+    lines=data.split("\n")[1:]
+    for line in lines:
+        name_from_file=line.split(",")[0]
+        if name==name_from_file:
+            found=1
+            return (line.split(",")[1],line.split(",")[2])
+    if(found==0):
+        return("NA","NA")
+
 
 @app.get("/sum/{username}")
 def index(username):
@@ -10,3 +27,13 @@ def index(username):
 @app.get("/sub")
 def movie():
     return {"sub":20}
+
+@app.get("/search/{name}")
+def getResults(name):
+    age,city=readFile(name)
+    if (age=="NA" and city=="NA"):
+        return {"response":f"user {name} not found in file"}
+    else:
+        return {"age":age,"city":city}
+
+
---
+++ Generic_Python/oops/test.py
@@ -0,0 +1,10 @@
+class Employee:
+  def __init__(self,age):
+    self.age=age
+  def display(self):
+    print(self.age)
+
+
+emp=Employee(20)
+emp.display()
+    
